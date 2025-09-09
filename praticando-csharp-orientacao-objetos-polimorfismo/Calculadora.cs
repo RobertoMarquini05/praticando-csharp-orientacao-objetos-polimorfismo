8e9897e0dd070eee3482469c0379a916a1f73f2e
Roberto Marquini <marquini.roberto05@gmail.com>
2025-09-09 17:03:16 -0300

    Desafio 01

---
+++ praticando-csharp-orientacao-objetos-polimorfismo/Calculadora.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praticando_csharp_orientacao_objetos_polimorfismo
+{
+    public class Calculadora
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Somar(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public double Somar(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ praticando-csharp-orientacao-objetos-polimorfismo/Program.cs
@@ -0,0 +1,11 @@
+﻿using praticando_csharp_orientacao_objetos_polimorfismo;
+
+Calculadora calc = new Calculadora();
+
+int resultado1 = calc.Somar(10, 20);
+int resultado2 = calc.Somar(5, 10, 15);
+double resultado3 = calc.Somar(3.5, 2.8);
+
+Console.WriteLine(resultado1); 
+Console.WriteLine(resultado2); 
+Console.WriteLine(resultado3); 
